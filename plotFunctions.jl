0eabbb59e22eeb30ff5d6a72bedfb48c06ffa034
Federica Milinanni <federica.milinanni@studenti.polito.it>
2021-08-25 19:22:44 +0200

    New Plots

--- plotFunctions.jl
+++ plotFunctions.jl
@@ -12,7 +12,7 @@ end
 function PlotSensitivityError(sol, S_PBS, S_FS_interp, S_Exp, Ti, Tf, Title, tsteps_exp)
     pl = plot(Ti:Tf,[map(t->(opnorm(S_PBS[t][:,:] - S_FS_interp[t][:,:])/opnorm(S_FS_interp[t][:,:])),Ti:Tf),
                 map(t->(opnorm(S_Exp[t][:,:] - S_FS_interp[t][:,:])/opnorm(S_FS_interp[t][:,:])),Ti:Tf)],
-                title=string(Title," - Relative error wrt FS"), label=["PBS" "Exp"], xlabel="time step",
+                title=string(Title," - Relative error wrt FS"), label=["PBSR" "Exp"], xlabel="time step",
                 yaxis=:log, legend=:bottomright , size = (900,600), palette=:grays1, linestyle=:auto)
     if !isempty(tsteps_exp)
         pl = plot!(tsteps_exp, map(t->(opnorm(S_PBS[t][:,:] - S_FS_interp[t][:,:])/opnorm(S_FS_interp[t][:,:])),tsteps_exp),  seriestype=:scatter, label=false,
