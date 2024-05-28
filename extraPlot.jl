fcf748e8f9b3e08e483394f3ab569bea07fdf6b5
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2024-05-28 14:30:31 +0200

    added Chua model to the extraPlots script

--- extraPlot.jl
+++ extraPlot.jl
@@ -3,6 +3,9 @@ include("plotFunctions.jl")
 abstolset = 1e-6;
 reltolset = 1e-5;
 
+##
+## PKA model
+##
 modelName = "PKA";
 include("./models/PKA.jl");
 tspan = (0.0,6000.0);
@@ -17,11 +20,14 @@ for j=1:length(R)
  plot!(R[j],fillcolor="blue",fillalpha=0.1,linecolor=RGBA(0,0,0,0),legend=false)
 end
 savefig("PKA.png");
+savefig("PKA.pdf");
 
-
+##
+## CaMKIIs model
+##
 modelName = "CaMKIIs";
 include("./models/CaMKIIs.jl");
-tspan = (0.0,600.0);
+tspan = (0.0,6000.0);
 
 S_PBS, timePBS, sol, timeStepsExp, N_intervals, indicator = PBSAlgorithm(f_ODE, x0, tspan, p);
 rE = relError(f_ODE, x0, tspan, p, sol, S_PBS)
@@ -33,5 +39,25 @@ for j=1:length(R)
  plot!(R[j],fillcolor="blue",fillalpha=0.1,linecolor=RGBA(0,0,0,0),legend=false)
 end
 savefig("CaMKIIs.png");
+savefig("CaMKIIs.pdf")
 
+
+##
+## ChuaCircuit model
+##
+modelName = "ChuaCircuit";
+include("./models/ChuaCircuit.jl");
+tspan = (0.0,100.0);
+
+S_PBS, timePBS, sol, timeStepsExp, N_intervals, indicator = PBSAlgorithm(f_ODE, x0, tspan, p);
+rE = relError(f_ODE, x0, tspan, p, sol, S_PBS)
+R = rectanglesFromIndicator(indicator,1:length(sol.t),1,1e-14,1e-7)
+ytk = (^).(10.0,-7:-1:-14)
+
+plot(rE,title="Linearization Error -- Chua's Circuit model",xlabel="time index",ylabel="estimated relative error",dpi=300,yaxis=(:log10,[1e-14,:auto]),ytick=ytk)
+for j=1:length(R)
+ plot!(R[j],fillcolor="blue",fillalpha=0.1,linecolor=RGBA(0,0,0,0),legend=false)
+end
+savefig("ChuaCircuit.png");
+savefig("ChuaCircuit.pdf")
 
